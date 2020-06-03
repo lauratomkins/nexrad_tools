c84c5ffc164e5ff1edddf22f777454ea96af8577
lauratomkins <lmtomkin@ncsu.edu>
2020-06-03 10:19:21 -0400

    Added directory creation

--- nexrad.py
+++ nexrad.py
@@ -6,12 +6,17 @@ Created on Fri Dec  6 16:03:52 2019
 """
 
 import nexradaws
+import os
 
-date = '20191210'
-radar = 'KOKX'
+date = '20200218'
+radar = 'KBGM'
+folder = 'C:\\Users\\lmtomkin\\Documents\\winter_storms\\data\\NEXRAD\\'+radar+'\\'+date
 
 conn = nexradaws.NexradAwsInterface()
 
 availscans = conn.get_avail_scans(date[0:4], date[4:6], date[6:8], radar)
 
-results = conn.download(availscans, 'C:\\Users\\lmtomkin\\Documents\\winter_storms\\data\\NEXRAD\\'+radar+'\\'+date)
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+results = conn.download(availscans, folder)
